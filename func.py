099089628e4e96bba02ae50f9156f0a0b9a4a3c2
Slanj <slanjr@gmail.com>
2018-05-18 02:33:48 +0300

    recursive and iterative functions added

--- func.py
+++ func.py
@@ -6,5 +6,39 @@ def hello(name = "Nikifor"):
     print(greet)
     return greet
 
+def iterPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+
+    returns: int or float, base^exp
+    '''
+    result = base
+    if exp == 0:
+        return 1
+    else:
+        for i in range(1, exp):
+            result *= base
+            print(result)
+    return result
+
+def recurPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+
+    returns: int or float, base^exp
+    '''
+    # Your code here
+
+    if exp == 1:
+        return base
+    elif exp == 0:
+        return 1
+    else:
+        return  base*recurPower(base, exp-1)
+
 hello()
 hello("Peter")
+
+iterPower(2, 4)
