7770c8b52dbf1d47d47cb9f1e678317c80d58961
Slanj <slanjr@gmail.com>
2018-07-10 20:45:39 +0300

    Class customization

--- class.py
+++ class.py
@@ -8,10 +8,26 @@ class Coordinate(object):
         y_diff_sq = (self.y - other.y)**2
         dist = sqrt(x_diff_sq + y_diff_sq)
         return dist
+    def __add__(self, other):
+        new_x = self.x + other.x
+        new_y = self.y + other.y
+        return Coordinate(new_x, new_y)
+    def __sub__(self, other):
+        new_x = self.x - other.x
+        new_y = self.y - other.y
+        return Coordinate(new_x, new_y)
+    def __str__(self):
+        return str((self.x, self.y))
 
 c = Coordinate(3, 4)
-origin = Coordinate(0, 0)
+origin = Coordinate(1, 1)
+sum_c = c + origin
 distance = c.distance(origin)
-print(c.x)
-print(origin.x)
+print(c)
+print(origin)
 print(distance)
+print(sum_c)
+print(type(c))
+print(type(Coordinate))
+print(isinstance(c, Coordinate))
+print(isinstance(sum_c, Coordinate))
