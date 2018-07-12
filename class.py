2117ca19d6fe19b10091920a49b6c2ebd773a7a5
Slanj <slanjr@gmail.com>
2018-07-12 16:13:09 +0300

    more class methods

--- class.py
+++ class.py
@@ -8,6 +8,13 @@ class Coordinate(object):
         y_diff_sq = (self.y - other.y)**2
         dist = sqrt(x_diff_sq + y_diff_sq)
         return dist
+    def getX(self):
+        # Getter method for a Coordinate object's x coordinate.
+        # Getter methods are better practice than just accessing an attribute directly
+        return self.x
+    def getY(self):
+        # Getter method for a Coordinate object's y coordinate
+        return self.y
     def __add__(self, other):
         new_x = self.x + other.x
         new_y = self.y + other.y
@@ -16,10 +23,15 @@ class Coordinate(object):
         new_x = self.x - other.x
         new_y = self.y - other.y
         return Coordinate(new_x, new_y)
+    def __eq__(self, other):
+        return (self.x == other.x) and (self.y == other.y)
+    def __repr__(self):
+        return 'Coordinate(' + str(self.x) + ',' + str(self.y) + ')'
     def __str__(self):
         return str((self.x, self.y))
 
 c = Coordinate(3, 4)
+cc = Coordinate(3, 4)
 origin = Coordinate(1, 1)
 sum_c = c + origin
 distance = c.distance(origin)
@@ -31,3 +43,6 @@ print(type(c))
 print(type(Coordinate))
 print(isinstance(c, Coordinate))
 print(isinstance(sum_c, Coordinate))
+print(c == origin)
+print(c == cc)
+print(eval(repr(c)) == c)
--- class_set.py
+++ class_set.py
@@ -1,4 +1,7 @@
 class intSet(object):
+    """An intSet is a set of integers
+    The value is represented by a list of ints, self.vals.
+    Each int in the set occurs in self.vals exactly once."""
     def __init__(self):
         self.vals = []
     def insert(self, e):
@@ -6,11 +9,19 @@ class intSet(object):
             self.vals.append(e)
     def member(self, e):
         return e in self.vals
+    def intersect(self, other):
+        selected = intSet()
+        for item in self.vals:
+            if other.member(item):
+                selected.insert(item)
+        return selected
     def remove(self, e):
         try:
             self.vals.remove(e)
         except:
             raise ValueError(str(e) + ' not found')
+    def __len__(self):
+        return len(self.vals)
     def __str__(self):
         self.vals.sort()
         result = ''
@@ -23,7 +34,17 @@ i.insert(5)
 i.insert(5)
 i.insert(77)
 i.insert(55)
-print(i)
+print("intSet i: ", i)
+
+m = intSet()
+m.insert(5)
+m.insert(55)
+print("intSet m: ", m)
+
+im = i.intersect(m)
+print(m, " is interselect between i and m")
+
+print("m len is: ", len(m))
 
 i.remove(55)
-print(i)
+print("Remove 55 from i:", i)
