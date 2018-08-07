c3334c3d671fb884bdc431358d628734cc0a9564
Slanj <slanjr@gmail.com>
2018-08-07 23:51:52 +0300

    somt sort and search

---
+++ search.py
@@ -0,0 +1,49 @@
+def search(L, e):
+    for i in range(len(L)):
+        if L[i] == e:
+            return True
+        if L[i] > e:
+            return False
+    return False
+
+def newsearch(L, e):
+    size = len(L)
+    for i in range(size):
+        if L[size-i-1] == e:
+            return True
+        if L[i] < e:
+            return False
+    return False
+
+def swapSort(L):
+    """ L is a list on integers """
+    print("Original L: ", L)
+    for i in range(len(L)):
+        for j in range(i+1, len(L)):
+            if L[j] < L[i]:
+                # the next line is a short
+                # form for swap L[i] and L[j]
+                L[j], L[i] = L[i], L[j]
+                print(L)
+    print("Final L: ", L)
+
+def modSwapSort(L):
+    """ L is a list on integers """
+    print("Original L: ", L)
+    for i in range(len(L)):
+        for j in range(len(L)):
+            if L[j] < L[i]:
+                # the next line is a short
+                # form for swap L[i] and L[j]
+                L[j], L[i] = L[i], L[j]
+                print(L)
+    print("Final L: ", L)
+
+L = [3, 5, 6, 6, 3]
+L2 = [3, 5, 6, 6, 3]
+e = 5
+
+print(search(L, e))
+print(newsearch(L, e))
+modSwapSort(L)
+swapSort(L2)
