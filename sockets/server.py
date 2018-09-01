f21433345ce82f4052a49c30ed81a9735365d274
Slanj <slanjr@gmail.com>
2018-09-02 02:53:00 +0300

    sockets simple examples

---
+++ sockets/server.py
@@ -0,0 +1,16 @@
+import socket
+
+sock = socket.socket()
+sock.bind(('', 9090))
+sock.listen(1)
+conn, addr = sock.accept()
+
+print('connected:', addr)
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.send(data.upper())
+
+conn.close()
