f5d1af6ad2c8e463493ad5f301268a2a071347c7
Slanj <slanjr@gmail.com>
2018-10-15 02:31:54 +0300

    Classification with KNN

---
+++ ml/classification.py
@@ -0,0 +1,79 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+import pandas as pd
+import numpy as np
+import matplotlib.ticker as ticker
+from sklearn import preprocessing
+
+df = pd.read_csv('teleCust1000t.csv')
+
+'''
+The target field, called custcat, has four possible values that correspond to the four customer groups, as follows: 1- Basic Service 2- E-Service 3- Plus Service 4- Total Service
+
+Our objective is to build a classifier, to predict the class of unknown cases. We will use a specific type of classification called K nearest neighbour.
+
+'''
+
+# Let’s see how many of each class is in our data set
+print(df['custcat'].value_counts())
+
+# df.hist(column='income', bins=50)
+# plt.show()
+
+# To use scikit-learn library, we have to convert the Pandas data frame to a Numpy array:
+X = df[['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ','retire', 'gender', 'reside']].values
+
+# Same conversion with other function
+# X1 = np.asanyarray(df[['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ','retire', 'gender', 'reside']])
+# print(X1[0:5])
+
+y = df['custcat'].values
+
+# Data Standardization give data zero mean and unit variance, it is good practice, especially for algorithms such as KNN which is based on distance of cases.
+
+X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
+# print(X[0:5])
+
+# Train test split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
+
+# Classifier implementing the k-nearest neighbors vote.
+from sklearn.neighbors import KNeighborsClassifier
+k = 6
+#Train Model and Predict
+neigh = KNeighborsClassifier(n_neighbors = k).fit(X_train,y_train)
+yhat = neigh.predict(X_test)
+
+from sklearn import metrics
+print("Train set Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(X_train)))
+print("Test set Accuracy: ", metrics.accuracy_score(y_test, yhat))
+
+# We can calculate the accuracy of KNN for different Ks.
+Ks = 10
+mean_acc = np.zeros((Ks-1))
+std_acc = np.zeros((Ks-1))
+ConfustionMx = [];
+for n in range(1,Ks):
+
+    #Train Model and Predict
+    neigh = KNeighborsClassifier(n_neighbors = n).fit(X_train,y_train)
+    yhat=neigh.predict(X_test)
+    mean_acc[n-1] = metrics.accuracy_score(y_test, yhat)
+
+
+    std_acc[n-1]=np.std(yhat==y_test)/np.sqrt(yhat.shape[0])
+
+plt.plot(range(1,Ks),mean_acc,'g')
+plt.fill_between(range(1,Ks),mean_acc - 1 * std_acc, mean_acc + 1 * std_acc, alpha=0.10)
+plt.legend(('Accuracy ', '+/- 3xstd'))
+plt.ylabel('Accuracy ')
+plt.xlabel('Number of Nabors (K)')
+plt.tight_layout()
+plt.show()
+
+print( "The best accuracy was with", mean_acc.max(), "with k =", mean_acc.argmax()+1)
